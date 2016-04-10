8010e0a5fb5880e22eb0ff2568f3659a180ee4f9
Katie Tarng <katietarng@gmail.com>
2016-04-10 16:03:06 -0700

    Finished functions exercise

--- functions.py
+++ functions.py
@@ -12,19 +12,16 @@ def print_hello():
 
 	print "Hello World"
 
-
-
-
 # 2. Write a function that takes a name as a string and
 #    prints "Hi" followed by the name.
+
+
+name = raw_input('What is your name? ')
 
 def greet_name(name):
 	"""Takes input and prints 'Hi', input"""
 
 	print 'Hi', name 
-
-# name = raw_input('What is your name? ')
-#Call function here - greet_name(name)
 
 
 # 3. Write a function that takes two integers and multiplies
@@ -91,7 +88,7 @@ def number_of_spaces(string_sentence):
 #    percentage should be optional; if not given, it should
 #    default to 15%.
 
-def tip_calculator(meal_price, tip_percent=15):
+def calculate_tip(meal_price, tip_percent=15):
 	"""Returns total amount paid with meal price and tip included.
 		If tip not included, tip defaults to 15%"""
 
@@ -132,15 +129,13 @@ def integer_info(integer):
 # 	variables --- sign and parity (whether it's positive
 # 	or negative). Print sign and parity.
 
-info_list = integer_info(integer) #calling function here
-	
-sign = info_list[0]
-parity = info_list[1]
+# info_list = integer_info(integer) #calling function here
 
-print sign
-print parity
+# sign = info_list[0]
+# parity = info_list[1]
 
-
+# print sign
+# print parity
 
 
 # ###############################################################
@@ -157,16 +152,70 @@ print parity
 #    If the state is California, apply a 7% tax within the function.
 #    Your function should return the total cost of the item including tax.
 
+def total_cost(price, state, tax = 0.05):
+	"""Calculating total cost of item. Tax parameter varies by state"""
+
+	if state == 'CA':
+		total = price + (0.07 * price)
+	else:
+		total = price + (tax * price)
+
+	return '{:.2f}'.format(total)
+
 # 2. Turn the block of code from the directions into a function.
 # 	 Take a name and a job title as parameters, making it so the
 # 	 job title defaults to "Engineer" if a job title is not passed in.
 # 	 Return the person's title and name.
+
+name = raw_input('What is your name? ')
+
+def print_job_title(name, job_title = 'Engineer'):
+	"""Return job title and name"""
+
+	return name,job_title
 
 # 3. Given a receiver's name, receiver's job title, and sender's name, print the following letter:      
 #       Dear JOB_TITLE RECEIVER_NAME, I think you are amazing! Sincerely,
 #       SENDER_NAME. 
 #    Use the function from #2 to construct the full title for the letter's greeting.
 
+receiver = print_job_title(name)
+r_name = receiver[0]
+r_job = receiver[1]
+
+def greeting_letter(receiver_name, receiver_job, sender_name):
+	"""Print greeting letter to receiver from print_job_title function 
+		and sign from sender"""
+
+	print 'Dear {} {}, I think you are amazing!\nSincerely, {}.'.format(receiver_job,
+																		receiver_name,
+																		sender_name)
+#Function call - greeting_letter(r_name,r_job,'Hackbright')
+
 # 4. Turn the block of code from the directions into a function. This
 #    function will take a number and append it to *numbers*. It doesn't
 #    need to return anything.
+
+numbers = []
+
+def add_list(num):
+	"""Add num to numbers list"""
+
+	numbers.append(num)
+
+	print numbers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
